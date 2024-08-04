029bdde8414fc022e0d8501834f22205cf16e8a6
仙 <110833020+tianhuzong@users.noreply.github.com>
2024-08-04 23:07:23 +0800

    Create codegen.h

---
+++ codegen.h
@@ -0,0 +1,7 @@
+static llvm::LLVMContext MyContext;
+class CodeGenContext {
+    public :
+        Module *module;
+        CodeGenContext(std::string module_name) {module = new Module(module_name,MyContext);}
+        void codeGen(std::vector<Node*> nodes);
+};
