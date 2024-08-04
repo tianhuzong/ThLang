8ffe565c71f6a0c57f6067282eb9fe61dcd39eb5
仙 <110833020+tianhuzong@users.noreply.github.com>
2024-08-04 23:05:39 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "nn.h"
+#include "codegen.h"
+#include "parser.hpp"
+using namespace std;
+extern std::vector<Node*> program;
+
+
+int main(int argc, char **argv){
+
+    yyparse();
+    CodeGenContext myC("main");
+
+}
+
